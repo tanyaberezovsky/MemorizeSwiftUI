9d1e16ec95ca2c147995f2a2ed7c3234669184c0
Tanya Berezovsky <tanyab@crow.co.il>
2020-11-21 21:29:50 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tanya Berezovsky on 21/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
