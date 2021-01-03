c1953b818e4aae813747f84aa97ab179aa464055
Tanya Berezovsky <tanyab@crow.co.il>
2021-01-03 19:58:35 +0200

    added preview

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack {
+            ForEach (0..<4) { _ in
+                CardView(isFaceUp: false)
+            }
+        }
+            
+            
+        .padding()
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
     }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke()
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView()        
     }
 }
